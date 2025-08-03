15f52005b81c6b8ada81809ddadebf2e3236edba
Hasanga Ranasinghe <dinithhasangare@gmail.com>
2025-08-03 14:00:00 +0530

    add metric function to calculate multiple evaluation metrics

--- projects/210515V-Time-Series_Univariate-Forecasting/src/utils/metrics.py
+++ projects/210515V-Time-Series_Univariate-Forecasting/src/utils/metrics.py
@@ -31,3 +31,15 @@ def MAPE(pred, true):
 def MSPE(pred, true):
     return np.mean(np.square((pred - true) / true))
 
+
+def metric(pred, true):
+    mae = MAE(pred, true)
+    mse = MSE(pred, true)
+    rmse = RMSE(pred, true)
+    mape = MAPE(pred, true)
+    mspe = MSPE(pred, true)
+    rse = RSE(pred, true)
+    corr = CORR(pred, true)
+
+    return mae, mse, rmse, mape, mspe, rse, corr
+
